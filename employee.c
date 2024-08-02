0de55585a0563facef1cd68381a51aed0ef3c6b7
Christopher Shea <chshea11@hotmail.com>
2024-08-01 23:58:32 -0300

    Init main function

---
+++ employee.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+
+void print_usage(char *argv[]){
+    printf("Usage: %s -n -f <database file>\n");
+    printf("\t -n - create new database file\n");
+    printf("\t -f - (required) path to database\n");
+}
+
+int main(int argc, char *argv[]) { 
+	char *filepath = NULL;
+	bool newfile = false;
+	int case;
+
+	while ((case = getopt(argc, argv, "nf:")) != -1) {
+		switch (case) {
+			case 'n':
+				newfile = true;
+				break;
+			case 'f':
+				filepath = optarg;
+				break;
+			case '?':
+				printf("Unknown option -%c\n", c);
+				break;
+			default:
+				return -1;
+		}
+	}
+}
